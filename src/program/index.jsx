07f80f00bbf436a3b1fd84d06a57d4f1f91342fb
Gui <guitoledo13@hotmail.com>
2025-11-01 22:56:44 -0300

    Styling and CSS

--- src/program/index.jsx
+++ src/program/index.jsx
@@ -1,18 +1,22 @@
+import styles from './program.module.css';
+
 export default () => {
     return (
     <>
-        <h1 className="title">B.M.I. CALCULATOR</h1>
-            <div className="input">
-                <ul className="inputItem">
-                    <li><h2>Enter your weight:</h2></li>
-                    <li><input type="text" placeholder="Weight"/></li>
-                </ul>
-                <ul className="inputItem">
-                    <li><h2>Enter your height:</h2></li>
-                    <li><input type="text" placeholder="Height"/></li>
-                </ul>
-            </div>
-        <h2 className="input2">Your B.M.I. is <span className="enter"></span></h2>
+    <div className={styles.contain}>
+        <h1 className={styles.title}>B.M.I. CALCULATOR</h1>
+                <div className={styles.input}>
+                    <ul className={styles.inputItemLeft}>
+                        <li><h2>Enter your weight:</h2></li>
+                        <li className={styles.liInput}><input className={styles.InputArea} type="text" placeholder="Weight"/></li>
+                    </ul>
+                    <ul className={styles.inputItemRight}>
+                        <li><h2>Enter your height:</h2></li>
+                        <li className={styles.liInput}><input className={styles.InputArea} type="text" placeholder="Height"/></li>
+                    </ul>
+                </div>
+        <h2 className="input2">Your B.M.I. is: <span className="enter"></span></h2>
+    </div>
     </>
     )
 }
--- src/tables/index.jsx
+++ src/tables/index.jsx
@@ -1,29 +1,29 @@
-
+import Styles from "./tablex.module.css";
 
 export default () => {
     return (
-        <ul className="table">
-            <ul className="tableLine">
+        <ul className={Styles.table}>
+            <ul className={Styles.tableLineTop}>
                 <li>Classification</li>
                 <li>I.M.C.</li>
             </ul>
-            <ul className="tableLine1">
+            <ul className={Styles.tableLine}>
                 <li> UnderWeight </li>
                 <li> Less than 18,5 </li>
             </ul>
-            <ul className="tableLine2">
+            <ul className={Styles.tableLine}>
                 <li> Healthy </li>
                 <li> 18,5 - 24,9 </li>
             </ul>
-            <ul className="tableLine3">
+            <ul className={Styles.tableLine}>
                 <li> OverWeight </li>
                 <li> 25,0 - 29,9 </li>
             </ul>
-            <ul className="tableLine4">
+            <ul className={Styles.tableLine}>
                 <li> Obese </li>
                 <li> 30,0 - 34,9 </li>
             </ul>
-            <ul className="tableLine5">
+            <ul className={Styles.tableLine}>
                 <li> Extremely Obese </li>
                 <li> More than 35 </li>
             </ul>
